188cf8f1375028af66fae80c5adbdc8f7d578ecf
AMonnierPro <43467458+AMonnierPro@users.noreply.github.com>
2020-02-01 17:36:39 +0100

    Calpin

--- Assets/Scripts/CalpinScript.cs
+++ Assets/Scripts/CalpinScript.cs
@@ -2,33 +2,115 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.SceneManagement;
 
 public class CalpinScript : MonoBehaviour
 {
 
     [SerializeField]
-    public GameObject  PrenomNom;
-    public Text  PrenomNomText;
+    public Text prenomNomText;
+    public Text line01;
+    public Text line02;
+    public Text line03;
+    public GameObject Page01;
+    public GameObject Page02;
+    public GameObject Page03;
+    public GameObject Page04;
 
-    string Text1;
-    string Text2;
-    string Text3;
-
-    string compteurpages;
     PatientInfo patient;
 
-    
-    // Start is called before the first frame update
+
     void Start()
     {
         patient = GetComponent<PatientInfo>();
-        PrenomNomText = PrenomNom.GetComponent<Text>();
+        //prenomNomText.text = patient.actualname + " " + patient.actualFirstname;
+        line01.text = "";
+        line02.text = "";
+        line03.text = "";
+        Page01.SetActive(true);
+        Page02.SetActive(false);
+        Page03.SetActive(false);
+        Page04.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PreviousPage()
     {
-       PrenomNomText.text = patient.actualname + " " + patient.actualFirstname; 
+        if (Page01.active == true)
+        {
+
+        }
+        else if (Page02.active == true)
+        {
+            Page01.SetActive(true);
+            Page02.SetActive(false);
+            Page03.SetActive(false);
+            Page04.SetActive(false);
+            line01.text = "";
+            line03.text = "";
+            line03.text = "";
+        }
+        else if (Page03.active == true)
+        {
+            Page01.SetActive(false);
+            Page02.SetActive(true);
+            Page03.SetActive(false);
+            Page04.SetActive(false);
+            line01.text = "";
+            line03.text = "";
+            line03.text = "";
+        }
+        else if (Page04.active == true)
+        {
+            Page01.SetActive(false);
+            Page02.SetActive(false);
+            Page03.SetActive(true);
+            Page04.SetActive(false);
+            line01.text = "";
+            line03.text = "";
+            line03.text = "";
+        }
+    }
+
+    public void NextPage()
+    {
+        if (Page01.active == true)
+        {
+            Page01.SetActive(false);
+            Page02.SetActive(true);
+            Page03.SetActive(false);
+            Page04.SetActive(false);
+            line01.text = "";
+            line03.text = "";
+            line03.text = "";
+        }
+        else if (Page02.active == true)
+        {
+            Page01.SetActive(false);
+            Page02.SetActive(false);
+            Page03.SetActive(true);
+            Page04.SetActive(false);
+            line01.text = "";
+            line03.text = "";
+            line03.text = "";
+        }
+        else if (Page03.active == true)
+        {
+            Page01.SetActive(false);
+            Page02.SetActive(false);
+            Page03.SetActive(false);
+            Page04.SetActive(true);
+            line01.text = "";
+            line03.text = "";
+            line03.text = "";
+        }
+        else if (Page04.active == true)
+        {
+
+        }
+    }
+
+    public void ShowHideCalpin()
+    {
+
     }
 }
