bc00b985dafc5a1a1ce21446e22fbd1bd5b6131d
loic <loic.disarbois@gmail.com>
2020-02-01 15:15:05 +0100

    ok

---
+++ Assets/Scripts/CalpinScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CalpinScript : MonoBehaviour
+{
+
+    [SerializeField]
+    public GameObject  PrenomNom;
+    public Text  PrenomNomText;
+
+    string Text1;
+    string Text2;
+    string Text3;
+
+    string compteurpages;
+    PatientInfo patient;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        patient = GetComponent<PatientInfo>();
+        PrenomNomText = PrenomNom.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       PrenomNomText.text = patient.actualname + " " + patient.actualFirstname; 
+    }
+}
