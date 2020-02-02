ae8e0093da562bc417c81531f02f9381637e6587
Ajirogg <raimundo.guillaume@gmail.com>
2020-02-02 14:19:44 +0100

    Merge branch 'ALan' into Guillaume_end
    # Conflicts:
    #	Assets/Scenes/SampleScene.unity
    #	Assets/Scripts/Backend.cs
    #	Assets/Scripts/Cursor.cs

---
+++ Assets/Scripts/Cursor_REMOTE_24336.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour
+{
+    [SerializeField] GameObject calpinMove = null;
+    [SerializeField] GameObject calpin03 = null;
+    [SerializeField] GameObject calpin04 = null;
+
+    private Ray ray;
+    private RaycastHit hitInfo;
+
+    public Backend back;
+
+
+    private void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            if (Input.GetMouseButton(0))
+            {
+                SetSateTrue(hitInfo.transform.tag);
+                if (hitInfo.transform.tag == "Psychologist")
+                {
+                    calpinMove.GetComponent<MoveCalpin>().ShowHideCalpin();
+                }
+            }
+
+        }
+    }
+
+    public void SetSateTrue(string tag)
+    {
+        ObjectUsable[] objs = back.patient.ObjectLister.objects;
+        foreach (ObjectUsable obj in objs)
+        {
+            if (obj.name == tag)
+            {
+                if (obj.state == false)
+                {
+                    obj.state = true;
+                    ApplyObjEffect(obj.name);
+                    //Debug.Log(obj.state);
+                }
+                else
+                {
+
+                }
+            }
+        }
+    }
+
+    public void ApplyObjEffect(string tag)
+    {
+        Patient actualPatient = back.patient;
+        ObjectUsable[] badobjs = back.patient.mentalDisorder.badObjects;
+        ObjectUsable[] goodobjs = back.patient.mentalDisorder.goodObjects;
+
+        foreach (ObjectUsable obj in badobjs)
+        {
+            if (obj.name == tag)
+            {
+                actualPatient.ClientMindHealthMin += obj.effect;
+                calpin03.GetComponent<CalpinScript>().EventCalpin(tag + " is bad");
+                calpin04.GetComponent<CalpinScript>().EventCalpin(tag + " is bad");
+            }
+        }
+        foreach (ObjectUsable mObj in goodobjs)
+        {
+            if (mObj.name == tag)
+            {
+                actualPatient.ClientMindHealthPlus += mObj.effect;
+                calpin03.GetComponent<CalpinScript>().EventCalpin(tag + " is good");
+                calpin04.GetComponent<CalpinScript>().EventCalpin(tag + " is good");
+            }
+        }
+        if (back.patient.mentalDisorder.neutralObject.name == tag)
+        {
+            calpin03.GetComponent<CalpinScript>().EventCalpin(tag + " is neutral");
+            calpin04.GetComponent<CalpinScript>().EventCalpin(tag + " is neutral");
+        }
+    }
+}
---
+++ Assets/Scripts/GameButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameButton : MonoBehaviour
+{
+    public void Home()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenuScene");
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+          Application.Quit();
+#endif
+    }
+}
